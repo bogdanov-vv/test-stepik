4180f5df8109c5bcf87ec17339e6a1d698b6bd3e
bogdanov-vv <bogdanov.itspec@gmail.com>
2020-06-18 19:15:18 +0300

    Repair fixture

--- test_items.py
+++ test_items.py
@@ -4,4 +4,4 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"#.format(language)
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
-    time.sleep(5)
+    # time.sleep(5)
