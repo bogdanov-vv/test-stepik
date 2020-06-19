16b137adb8f6db1d9d6a9498bb6d01ca20f6ab57
bogdanov-vv <bogdanov.itspec@gmail.com>
2020-06-19 13:07:26 +0300

    Rename function test

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_see_button_buy(browser):
+def test_items(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     assert WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary btn-add-to-basket"]')))
