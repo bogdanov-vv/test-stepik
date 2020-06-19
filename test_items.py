f1568fb869f036e7f7bfc0f7c003339892315166
bogdanov-vv <bogdanov.itspec@gmail.com>
2020-06-19 14:23:20 +0300

    Add verify locator try\except

--- test_items.py
+++ test_items.py
@@ -5,4 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_items(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    assert WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary btn-add-to-basket"]')))
+    # Проверка локатора
+    try:
+        browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+        bt = True
+    except:
+        bt = False
+    assert bt, 'Button "Add to basket" not found!'
+
+    # Простое решение проверки локатора, падает при плохом селекторе.
+    # button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary btn-add-to-basket"]')))
+    # assert button, 'Button "Add to basket" not found!'
